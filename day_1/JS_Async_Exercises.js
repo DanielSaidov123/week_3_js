fd388f046c7bbcc22882ee4893107495ee2969e6
Daniel_Saidov <ddny3622@gmail.com>
2025-12-14 19:58:26 +0200

    add func

--- day_1/JS_Async_Exercises.js
+++ day_1/JS_Async_Exercises.js
@@ -1,33 +1,69 @@
 import fs from "node:fs";
+import path from "node:path";
 function readFile(path) {
   fs.readFile(path, "utf-8", (err, data) => {
     if (err) {
       console.log(err);
     } else {
-        statFile(path)
-       
+      console.log(data);
+
+      fs.stat(path, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("size: ", data.size, "data: ", data.birthtime);
+        }
+      });
     }
   });
 }
 
-function statFile(path) {
-  fs.stat(path, (err, data) => {
+function listFilesOnly(dirPath) {
+  fs.readdir(dirPath, (err, items) => {
+    if (err) {
+      console.log("Error reading directory:", err);
+      return;
+    }
+    console.log(items);
+
+    items.forEach((item) => {
+      const fullPath = path.join(dirPath, item);
+
+      fs.lstat(fullPath, (err, stats) => {
+        if (err) {
+          console.log("Error getting stats for", item, err);
+          return;
+        }
+
+        if (stats.isFile()) {
+          console.log("File:", item);
+        }
+      });
+    });
+  });
+}
+
+function printCountdown(n) {
+  if (n <= 0) {
+    console.log("Time's up!");
+  } else {
+    console.log(n);
+    setTimeout(() => printCountdown(n - 1), 1000);
+  }
+}
+
+function readerFile(Path) {
+  fs.readFile(Path, "utf8", (err, data) => {
     if (err) {
       console.log(err);
-    } else {
-      console.log('size: ',data.size,'data: ',data.birthtime);
+      return;
+    }else{
+      const dataSplit=data.split("\n")
+      dataSplit.filter(line => line.trim() !== "")
+      console.log(dataSplit);
+      
+      
     }
   });
 }
-readFile('daniel.txt')
-
-// function lstatFile(path){
-//     fs.lstat(path, (err, data) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log(data.isFile());
-//     }
-//   });
-//  } 
-// lstatFile('daniel.txt')
+readerFile('ff/dd.txt')
